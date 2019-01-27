61c2820eae724e191c56f6a41f5fe21c4b548ac6
dbhautik <dbhavtik@gmail.com>
2019-01-27 23:32:47 +0530

    Start Developing

---
+++ disable-users.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Disable Users
+Plugin URI:   https://example.com/plugins/the-basics/
+Description:  Disable user from login into wordpress site.
+Version:      0.0.1
+Author:       dbhautik
+Author URI:   www.dbhautik.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  disuse
+Domain Path:  /languages
+*/
